e173b125f64b5375c06bead349a5fb089c4469db
ゆーせー <hirame0427@gmail.com>
2026-08-09 20:05:22 +0900

    スパマ側のPIDにしてみた

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,12 +5,12 @@
 package frc.robot;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.PersistMode;
-import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkMax;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 
@@ -24,9 +24,10 @@ public class Robot extends TimedRobot {
   private SparkMax lShooter;
   private SparkMax rShooter;
   private XboxController controller;
-  private PIDController pid;
-  private RelativeEncoder encoder;
-  private double MaxRPM = 3000;
+  private SparkClosedLoopController leftShooterController;
+  private SparkClosedLoopController rightShooterController;
+  private static final double MAX_RPM = 3000.0;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -36,24 +37,33 @@ public class Robot extends TimedRobot {
     lShooter = new SparkMax(18, MotorType.kBrushless);
     rShooter = new SparkMax(17, MotorType.kBrushless);
     controller = new XboxController(0);
-    pid = new PIDController(0.0001, 0, 0);
-    SparkMaxConfig Config = new SparkMaxConfig();
-    Config.inverted(true);
-    lShooter.configure(Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-  
+
+    // Configure the PID loop that runs on the Spark MAX, using its built-in encoder for feedback.
+    SparkMaxConfig leftConfig = new SparkMaxConfig();
+    leftConfig
+        .inverted(true)
+        .closedLoop
+        .pid(0.0001, 0.0, 0.0);
+    lShooter.configure(leftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // Only the left shooter is inverted. The right shooter uses the same PID gains normally.
+    SparkMaxConfig rightConfig = new SparkMaxConfig();
+    rightConfig.closedLoop.pid(0.0001, 0.0, 0.0);
+    rShooter.configure(rightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    leftShooterController = lShooter.getClosedLoopController();
+    rightShooterController = rShooter.getClosedLoopController();
   }
 
   /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
     if (controller.getXButton()) {
-      double output = pid.calculate(encoder.getVelocity(), MaxRPM);
-      lShooter.set(output);
-      rShooter.set(output);
+      leftShooterController.setSetpoint(MAX_RPM, ControlType.kVelocity);
+      rightShooterController.setSetpoint(MAX_RPM, ControlType.kVelocity);
     } else {
       lShooter.set(0.0);
       rShooter.set(0.0);
-      pid.reset();
     }
   }
 }
